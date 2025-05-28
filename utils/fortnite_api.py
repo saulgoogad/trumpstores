c6a6dc8f8fd92746561a6fd8a705cdea50f572c2
saulgoogad <Pixeloffload@gmail.com>
2025-05-28 20:23:10 +0500

    Update fortnite_api.py

--- utils/fortnite_api.py
+++ utils/fortnite_api.py
@@ -3,9 +3,17 @@ import requests
 def get_shop_data():
     url = "https://fortnite-api.com/v2/shop/br?language=ru"
     response = requests.get(url)
-    data = response.json()
-    items = []
+    
+    if response.status_code != 200:
+        print("Fortnite API Error:", response.status_code)
+        return []
 
+    data = response.json()
+    if 'data' not in data or 'featured' not in data['data']:
+        print("Fortnite API response structure is invalid.")
+        return []
+
+    items = []
     for entry in data['data']['featured']['entries']:
         for item in entry['items']:
             items.append({
